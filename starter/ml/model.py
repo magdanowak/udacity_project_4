ffa825521bd3003b064375e79729d9284f41e95d
magdanowak <magdanowak@users.noreply.github.com>
2024-05-20 13:34:38 +0200

    fill out functions

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,5 +1,5 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-
+from sklearn.ensemble import RandomForestClassifier
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -17,8 +17,14 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
+    model = RandomForestClassifier(
+        n_estimators=50,
+        max_depth=10,
+        min_samples_leaf=5
+    )
+    model.fit(X_train, y_train)
+    return model
 
-    pass
 
 
 def compute_model_metrics(y, preds):
@@ -48,7 +54,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : RandomForestClassifier
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -57,4 +63,4 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    return model.predict(X)
--- starter/train_model.py
+++ starter/train_model.py
@@ -1,10 +1,15 @@
 # Script to train machine learning model.
+import pickle
 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+from starter.ml.data import process_data
+from starter.ml.model import train_model
 
 # Add code to load in the data.
+data = pd.read_csv("data/census.csv")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -24,5 +29,17 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, _, _ = process_data(
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb
+)
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+
+with open("model/model.pkl", "wb") as file:
+    pickle.dump(model, file)
